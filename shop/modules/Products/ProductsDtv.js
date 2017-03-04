a7bee378dfa97e367af795a6d94e3a9044f58cfb
Alejandro <alejandromartinezap@gmail.com>
2017-03-04 14:29:51 -0300

    Git ignore, products detail view refactored

--- shop/modules/Products/ProductsDtv.js
+++ shop/modules/Products/ProductsDtv.js
@@ -11,8 +11,10 @@ export class ProductsDtv {
     }
 
     link(scope, element, attrs) {
-    	scope.productsSvc.getProductPictures( attrs.itemid ).then(( product ) => {
-    		scope.image = product.data.pictures[0].secure_url;
+    	
+    	scope.productsSvc.getProductPictures( scope.$parent.product.id ).then(( product ) => {
+    		scope.$parent.product.image = product.data.pictures[0].secure_url;
+    		scope.product = scope.$parent.product;
     	})
     }
 }
